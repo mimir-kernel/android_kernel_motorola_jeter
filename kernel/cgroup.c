584fe930caea8421475b0e78c142dbc2d611a9fb
SaaSD3v <hearesaas@gmail.com>
2026-06-26 20:58:33 -0400

    Droidspaces/LXC: fix: restore cgroup file prefix handling
    for Droidspaces/LXC compatibility
    
    [Vhmit]: Adapted for 3.18
    
    - ref: https://github.com/ravindu644/Droidspaces-OSS/blob/main/Documentation/resources/kernel-patches/non-GKI/02.fix_restore%20cgroup%20file%20prefix%20handling%20.patch
    
    Signed-off-by: SaaSD3v <hearesaas@gmail.com>

--- kernel/cgroup.c
+++ kernel/cgroup.c
@@ -3094,6 +3094,18 @@ static int cgroup_add_file(struct cgroup *cgrp, struct cftype *cft)
 		return ret;
 	}
 
+	/*
+	 * Android mounts some cgroup controllers with "noprefix". Keep the
+	 * legacy name, but also expose the prefixed alias expected by container
+	 * runtimes such as DroidSpaces and LXC.
+	 */
+	if (cft->ss && (cgrp->root->flags & CGRP_ROOT_NOPREFIX) &&
+	    !(cft->flags & CFTYPE_NO_PREFIX)) {
+		snprintf(name, CGROUP_FILE_NAME_MAX, "%s.%s",
+			 cft->ss->name, cft->name);
+		kernfs_create_link(cgrp->kn, name, kn);
+	}
+
 	if (cft->seq_show == cgroup_populated_show)
 		cgrp->populated_kn = kn;
 	return 0;
@@ -3741,11 +3753,7 @@ int cgroup_transfer_tasks(struct cgroup *to, struct cgroup *from)
 	 */
 	do {
 		css_task_iter_start(&from->self, &it);
-
-		do {
-			task = css_task_iter_next(&it);
-		} while (task && (task->flags & PF_EXITING));
-
+		task = css_task_iter_next(&it);
 		if (task)
 			get_task_struct(task);
 		css_task_iter_end(&it);
